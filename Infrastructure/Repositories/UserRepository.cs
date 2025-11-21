c3974687d53cac4a4b77023cc476794b8db906b7
Husniddin <abdukholiqovh@gmail.com>
2025-11-21 11:22:37 +0500

    feat: Added UserRepository

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Persistence.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+public class UserRepository(AppDbContext context) : IUserRepository
+{
+    private readonly AppDbContext _context = context;
+    public async Task CreateAsync(User user, CancellationToken cancellationToken) =>
+        await _context.Users.AddAsync(user, cancellationToken);
+
+    public async Task<bool> ExistsAsync(string usernameOrEmail) =>
+        await _context.Users.AnyAsync(u => (u.Username == usernameOrEmail
+        || u.Email == usernameOrEmail) && u.IsActive);
+}
